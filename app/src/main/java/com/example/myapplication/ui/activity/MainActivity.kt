d82ffa90ecdb7efc92bae9987821fee26bb40f1c
DzhakypovAliaskar <dzhakypov007@gmail.com>
2022-03-04 15:50:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.myapplication.ui.activity
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getImage()
+        getCalculation()
+    }
+
+    private fun getCalculation() {
+                     binding.textEd.addTextChangedListener(object: TextWatcher{
+                         override fun beforeTextChanged(
+                             p0: CharSequence?,
+                             p1: Int,
+                             p2: Int,
+                             p3: Int
+                         ) {
+                         }
+
+                         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                             binding.numberTv.text = p0?.length.toString()
+                         }
+
+                         override fun afterTextChanged(p0: Editable?) {
+                         }
+                     })
+    }
+
+    private fun getImage() {
+        binding.btnSearch.setOnClickListener {
+            Glide.with(binding.imageCat).load("https://http.cat/${binding.textEd.text}")
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.VISIBLE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        binding.progressBar.visibility = View.GONE
+                        return false
+                    }
+                }).into(binding.imageCat)
+        }
+    }
+}
